2cd3438ae5148d7b3163519764745118f9509e68
Xu Min <xmin23@gmail.com>
2021-11-22 15:41:04 +0800

    Change dashboard

--- 2103/dashboard.php
+++ 2103/dashboard.php
@@ -25,31 +25,7 @@
         ?>
     </head>
     <body>
-<<<<<<< HEAD
         <?php include "nav-top.inc.php"; ?>
-=======
-        <nav class="navbar navbar-light bg-light p-3">
-            <div class="d-flex col-12 col-md-3 col-lg-2 mb-2 mb-lg-0 flex-wrap flex-md-nowrap justify-content-between">
-                <a class="navbar-brand" href="#">
-                    Weathercise Dashboard
-                </a>
-                <button class="navbar-toggler d-md-none collapsed mb-3" type="button" data-toggle="collapse" data-target="#sidebar" aria-controls="sidebar" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-            </div>
-            <div class="col-12 col-md-5 col-lg-8 d-flex align-items-center justify-content-md-end mt-3 mt-md-0">
-                <div class="dropdown">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-expanded="false">
-                        Hello, <?php echo $_SESSION['Username'] ?>
-                    </button>
-                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <li><a class="dropdown-item" href="/ICT2103/update_profile.php">Update Profile</a></li>
-                        <li><a class="dropdown-item" href="/ICT2103/logout.php">Sign out</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
->>>>>>> 7d7c6c22d8a765c853a189171ffba12269d36088
         <div class="container-fluid">
             <div class="row">
                 <nav id="sidebar" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
