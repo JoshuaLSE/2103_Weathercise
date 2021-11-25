6569dc2f9aa099eef2b0bad7be66db42920e0e4e
Xu Min <xmin23@gmail.com>
2021-11-25 21:40:36 +0800

    signup_form.php change to nosql

--- 2103/signup_form.php
+++ 2103/signup_form.php
@@ -4,10 +4,7 @@ session_start();
 $server = "mongodb+srv://admin:Passw0rd@ict2103.jbggf.mongodb.net/test?authSource=admin&replicaSet=atlas-lie30k-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true";
 $client = new MongoDB\Driver\Manager($server);
 $bulk = new MongoDB\Driver\BulkWrite;
-#constructing the querry
-//$query = new MongoDB\Driver\Query($filter, $options);
 
-    //$cursor = $client->executeQuery('ICT2103.users', $query);
     $firstname = $_POST['firstname'];
     $lastname = $_POST['lastname'];
     $height = $_POST['height'];
@@ -23,12 +20,8 @@ $stats = new MongoDB\Driver\Query($filter);
 
 $res = $client->executeQuery("ICT2103.user", $stats);
 if (empty(current($res->toArray()))) {
-    // UserName not inside
-    header("Location:signup.php?error=2");
-}else{
-    //UserName not inside
-}
-    if ($pword == $cpword) {
+    // UserName not inside Can create new user
+        if ($pword == $cpword) {
             $doc =[
                 'Password' => $pword,
                 'UserName' =>$username,
@@ -53,30 +46,10 @@ if (empty(current($res->toArray()))) {
         } else {
             header("Location:signup.php?error=1");
         }
-
-?>
-<?php
-
-$username = $_POST['username'];
-$password = $_POST['password'];
-$filter = ["UserName" => $username, "Password" => $password];
-
-$server = "mongodb+srv://admin:Passw0rd@ict2103.jbggf.mongodb.net/test?authSource=admin&replicaSet=atlas-lie30k-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true";
-$client = new MongoDB\Driver\Manager($server);
-//$options = ['find'=>array('password'=>$password)]; # limit -1 from newest to oldest
-
-$stats = new MongoDB\Driver\Query($filter);
-
-$res = $client->executeQuery("ICT2103.user", $stats);
-if (empty(current($res->toArray()))) {
-    header("Location:login.php?fail=1");
-} else {
-    session_start();
-
-    echo $_SESSION['Username'] = $username;
-    //echo $_SESSION['ID']= $nameid;
-
-    header("Location:dashboard.php");
+}else{
+    //UserName inside
+    header("Location:signup.php?error=2");
 }
-?>
 
+
+?>
